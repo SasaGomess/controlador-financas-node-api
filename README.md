426fe32cb27a03045e7a586719b24bbde21e2fc2
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-01-31 21:46:12 -0300

    chore: add dependencias iniciais do projeto e estrutura inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# Controlador de finanças
---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config();
+
+const server = express();
+
+server.use(express.json());
+
+server.use("/", (req, res) => {
+    res.send("Conexão estabelecida com sucesso!");
+})
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log(`Servidor escutando na porta ${process.env.PORT}`)
+})
