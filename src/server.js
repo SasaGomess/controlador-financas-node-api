25d52ba85c827e4f67eddb4605552c0c591810ee
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-04 15:40:39 -0300

    feat: adiciona Dockerfile para conteinerização da api e adiciona o serviço Redis para implemetação de cache

--- src/server.js
+++ src/server.js
@@ -16,6 +16,6 @@ server.use("/", (req, res) => {
     res.send("Conexão estabelecida com sucesso!");
 })
 
-server.listen(process.env.PORT || 3000, () => {
-    console.log(`Servidor escutando na porta ${process.env.PORT}`)
+server.listen(process.env.LOCAL_PORT || 3000, () => {
+    console.log(`Servidor escutando na porta ${process.env.LOCAL_PORT}`)
 })
