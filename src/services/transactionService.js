4ca014af00c89ffbc4f468c5fb373dad6f1005fd
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-04 02:05:02 -0300

    feat: add dto para não transferir dados sensíveis

--- src/services/transactionService.js
+++ src/services/transactionService.js
@@ -1,81 +1,94 @@
 const prisma = require("../utils/adapter");
 
-async function create(data, userId){
-   try{
-       const transactionCreated = await prisma.transaction.create({
-       data: {
-           description: data.description,
-           type: data.type,
-           date: data.date, 
-           expenseCategory: data.expenseCategory,
-           incomeCategory: data.incomeCategory,
-           amount: parseFloat(data.amount),
-           userId: userId
-        }
-    })
-
-    return transactionCreated;
-
-    } catch (error) {
-        console.log("Erro ao criar uma transação: " + error)
-        return null;
-    }
-}
-
-async function find(id, userId){
-    try{
-        const transaction = await prisma.transaction.findUnique({where: {id, userId}});
-        
-        if(!transaction){
-            return null
-        }
-
-        return transaction;
-    }catch (error){
-      console.log("Erro ao encontrar uma transação" + error);
-    }
-}
-
-async function findAll(userId){
-    try{
-       const transactions = await prisma.transaction.findMany({where: {userId}});
-
-       return transactions;
-    } catch (error) {
-        console.log("Erro ao encontrar transações: " + error);
-    }
-}
-
-async function deleteTransaction(transaction){
-    try{
-       await prisma.transaction.delete({where: {id: transaction.id}});
-    } catch (error){
-        console.log("Erro ao deletar o usuário: " + error);
-    }
-}
-
-async function update(id, transaction){
-    try{
-    
-       const transactionUpdated = prisma.transaction.update({where: {id},
-          data: {
-            amount: parseFloat(transaction.amount),
-            date: transaction.date,
-            description: transaction.description
-        }
-
+async function create(data, userId) {
+  try {
+    const transactionCreated = await prisma.transaction.create({
+      data: {
+        description: data.description,
+        type: data.type,
+        date: data.date,
+        expenseCategory: data.expenseCategory,
+        incomeCategory: data.incomeCategory,
+        amount: parseFloat(data.amount),
+        userId: userId,
+      },
     });
-     return transactionUpdated;
-    } catch (error) {
-        console.log("Error ao atualizar uma transação")
-    }
+
+    return {
+      description: transactionCreated.description,
+      type: transactionCreated.type,
+      expenseCategory: transactionCreated.expenseCategory,
+      incomeCategory: transactionCreated.incomeCategory,
+      amount: transactionCreated.amount,
+    };
+  } catch (error) {
+    console.log("Erro ao criar uma transação: " + error);
+    return null;
+  }
 }
 
+async function find(id, userId) {
+  try {
+    const transaction = await prisma.transaction.findUnique({
+      where: { id, userId },
+    });
+
+    if (!transaction) {
+      return null;
+    }
+
+    return transaction;
+  } catch (error) {
+    console.log("Erro ao encontrar uma transação" + error);
+  }
+}
+
+async function findAll(userId) {
+  try {
+    const transactions = await prisma.transaction.findMany({
+      where: { userId },
+    });
+
+    return transactions;
+  } catch (error) {
+    console.log("Erro ao encontrar transações: " + error);
+  }
+}
+
+async function deleteTransaction(transaction) {
+  try {
+    await prisma.transaction.delete({ where: { id: transaction.id } });
+  } catch (error) {
+    console.log("Erro ao deletar o usuário: " + error);
+  }
+}
+
+async function update(id, transaction) {
+  try {
+    const transactionUpdated = prisma.transaction.update({
+      where: { id },
+      data: {
+        amount: parseFloat(transaction.amount),
+        date: transaction.date,
+        description: transaction.description,
+      },
+    });
+    return{
+      description: transactionUpdated.description,
+      type: transactionUpdated.type,
+      expenseCategory: transactionUpdated.expenseCategory,
+      incomeCategory: transactionUpdated.incomeCategory,
+      amount: transactionUpdated.amount
+    };
+  } catch (error) {
+    console.log("Error ao atualizar uma transação");
+  }
+}
 
 module.exports = {
-    find,
-    create,
-    findAll,
-    deleteTransaction,
-    update
-}
+  find,
+  create,
+  findAll,
+  deleteTransaction,
+  update,
+};
